7816c903109add1769e281882be397670c6694ea
JonMid <jonmid.mideros@gmail.com>
2019-02-06 08:33:05 -0500

    Add project files.

---
+++ CsharpWebService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpWebService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
